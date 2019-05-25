66a38e97f4e3feb511d6684f89ee3f328b94fd05
Dan Isaza <daniel.isaza.93@gmail.com>
2019-05-25 00:39:19 -0500

    initial commit

--- src/components/header.js
+++ src/components/header.js
@@ -5,22 +5,26 @@ import React from 'react'
 const Header = ({ siteTitle }) => (
   <div
     style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
+      background: `none`,
+      marginBottom: `0.2rem`,
     }}
   >
     <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        padding: `3rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1
+        style={{
+          margin: 0,
+        }}
+      >
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `#444444`,
             textDecoration: `none`,
           }}
         >
--- src/components/layout.js
+++ src/components/layout.js
@@ -28,11 +28,6 @@ const Layout = ({ children }) => (
           }}
         >
           {children}
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
         </div>
       </>
     )}
