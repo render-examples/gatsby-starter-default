191ec2335cb845c7b81d8b36dcd8dad65ed77800
Dan Isaza <daniel.isaza.93@gmail.com>
2019-05-25 01:27:52 -0500

    update homepage copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,16 +13,22 @@ const IndexPage = () => (
       devops.
     </p>
     <p>
-      I'm the Head of Engineering at{' '}
-      <a href="https://listwithclever.com/">Clever Real Estate</a>.
+      I'm currently the Head of Engineering at{' '}
+      <a href="https://listwithclever.com/">Clever Real Estate</a>. I studied
+      Math & CS at Stanford.
     </p>
     <p>
-      My other interests include <Link to="/reading">reading</Link>,{' '}
+      My interests include <Link to="/reading">reading</Link>,{' '}
       <a href="https://brutalist-web.design/">brutalist web design</a>, and{' '}
       <a href="https://github.com/PlotterClub/penplotPlayground">
         plotting generative art
       </a>
       .
+    </p>
+    <p>
+      You can learn more about me on{' '}
+      <a href="https://brutalist-web.design/">LinkedIn</a>, and – if you're good
+      at guessing – you're welcome to email me.
     </p>
   </Layout>
 )
